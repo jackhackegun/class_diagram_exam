b92007471c4281a79a620ac1a282b4decd14ef55
김민서 <25_11@bssm.hs.kr>
2026-04-09 15:38:19 +0900

    first commit

--- exam.java
+++ exam.java
@@ -1,1 +1,25 @@
+package class_diagram_exam;
 
+class Character {
+    Potion mypotion;
+    Weapon myweapon;
+
+    Character() {
+    }
+
+    void getWeapon(Weapon weapon) {
+        this.myweapon = weapon;
+    }
+}
+
+class Potion {
+
+}
+
+class Monster {
+
+}
+
+class Weapon {
+
+}
